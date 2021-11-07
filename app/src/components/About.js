54ed0f417e27b535f840268ff0e9eea14ab86a25
bighappyface <anomalous20@gmail.com>
2021-11-07 13:28:12 -0500

    Update about

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -41,20 +41,20 @@ export default function About() {
       <Typography className={classes.question}>Where are the token assets stored?</Typography>
       <Typography>Both the NFT metadata and Puppee images are stored and pinned on IPFS.</Typography>
 
-      <Typography className={classes.question}>What make this project unique?</Typography>
+      <Typography className={classes.question}>What makes this token unique?</Typography>
       <Typography>Puppeeth uses a simple algorithm to control the token supply and valid ID values.</Typography>
       <Typography>The algorithm is a <em>pure</em> function that allows for the <u>token mint to consume less gas than a token transfer</u>!</Typography>
 
-      <Typography className={classes.question}>What make a Puppee unique?</Typography>
+      <Typography className={classes.question}>What makes a Puppee unique?</Typography>
       <Typography>Each number in the Puppee ID indicates a specific attribute. Each attribute is 1 in 5, but attribute combinations make each Puppee more rare than others.</Typography>
-      <Typography>For example, there are only 100 Puppees with only one number different than the others (e.g. 51111, 44344, 23222).</Typography>
+      <Typography>For example, there are only 100 Puppees with one number different than the others (e.g. 51111, 44344, 23222).</Typography>
       <Typography>Find a unique combination before they are all minted!</Typography>
 
-      <Typography className={classes.question}>How many can I mint at once?</Typography>
+      <Typography className={classes.question}>How many tokens can I mint at once?</Typography>
       <Typography>Tokens can be minted one at a time. This was decided to allow buyers a fair shot at a Puppee they like.</Typography>
 
       <Typography className={classes.question}>How many tokens did the creators get?</Typography>
-      <Typography>We minted 11 tokens when the contract was deployed, for friends and family. All new tokens must be minted the same as all others.</Typography>
+      <Typography>We minted 11 tokens when the contract was deployed, for friends and family. All new tokens must be minted through the contract, for everyone.</Typography>
     </Grid>
     </>
   );
